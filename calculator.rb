c353336196406a8ab10cf170210696e466c3654e
miniengineer <minira.samadova@3ds.com>
2022-06-20 10:20:22 +0900

    unsuccsessful .and_call_original test

---
+++ calculator.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Calculator
+    class << self
+        def add(num1, num2)
+            num1 + num2
+        end
+    
+        def multiply(num1, num2)
+            num1 * num2
+        end
+    end
+end
+
---
+++ spec/counter_spec.rb
@@ -0,0 +1,11 @@
+require "./calculator"
+
+RSpec.describe "and_call_original" do
+    it "trying to figure out what it does" do
+      allow(Calculator).to receive(:add).and_call_original
+      allow(Calculator).to receive(:add).with(2, 2).and_return(-3)
+  
+      expect(Calculator.add(2, 2)).to eq(4)
+      expect(Calculator.add(2, 2)).to eq(-3)
+    end
+  end
