1cd6a0ab0157bea36bb0ade7227c7841db04cbac
miniengineer <minira.samadova@3ds.com>
2022-06-20 17:40:54 +0900

    two spaces instead of four

--- and_call_original/calculator.rb
+++ and_call_original/calculator.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module Calculator
-    class << self
-        def add(num1, num2)
-            num1 + num2
-        end
-    
-        def multiply(num1, num2)
-            num1 * num2
-        end
+  class << self
+    def add(num1, num2)
+      num1 + num2
     end
+
+    def multiply(num1, num2)
+      num1 * num2
+    end
+  end
 end
