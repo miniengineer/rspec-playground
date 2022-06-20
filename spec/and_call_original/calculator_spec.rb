4954c449ca5be19a2a3aefae6a49e5087fdaa543
miniengineer <minira.samadova@3ds.com>
2022-06-20 17:39:50 +0900

    call except with different arguments

--- spec/and_call_original/calculator_spec.rb
+++ spec/and_call_original/calculator_spec.rb
@@ -1,11 +1,11 @@
 require "../and_call_original/calculator"
 
 RSpec.describe "and_call_original" do
-    it "trying to figure out what it does" do
-      allow(Calculator).to receive(:add).and_call_original
-      allow(Calculator).to receive(:add).with(2, 2).and_return(-3)
-  
-      expect(Calculator.add(2, 2)).to eq(4)
-      expect(Calculator.add(2, 2)).to eq(-3)
-    end
+  it "trying to figure out what it does" do
+    allow(Calculator).to receive(:add).and_call_original
+    allow(Calculator).to receive(:add).with(2, 3).and_return(-5)
+
+    expect(Calculator.add(2, 2)).to eq(4)
+    expect(Calculator.add(2, 3)).to eq(-5)
   end
+end
